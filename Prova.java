a3461bce394701d24717e8feff8a400842ef2c85
Josep Roure Alcobé <josep@MacBook-Pro-de-Josep.local>
2024-07-08 11:59:36 +0200

    Canvis fets pel Josep

--- Prova.java
+++ Prova.java
@@ -1,5 +1,7 @@
-public class Proba {
+public class Prova {
     public static void main(String[] args) {
+
         System.out.println("Hello, World!");
+        System.out.println("Geniall!!!");
     }
 }
